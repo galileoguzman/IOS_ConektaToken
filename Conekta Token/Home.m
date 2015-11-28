1ff802136a7a564fe2c79230ac5ecb3090af6668
Galileo Guzmán <galileoguzman@gmail.com>
2015-11-28 11:06:19 -0600

    Test conekta services

--- Conekta Token/Home.m
+++ Conekta Token/Home.m
@@ -7,6 +7,7 @@
 //
 
 #import "Home.h"
+#import "Conekta.h"
 
 @interface Home ()
 
@@ -15,13 +16,33 @@
 @implementation Home
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) getToken
+{
+    Conekta *conekta = [[Conekta alloc] init];
+    [conekta setDelegate: self];
+    [conekta setPublicKey:@"your_public_key"];
+    [conekta collectDevice];
+    
+    
+    // create objecti type conekta
+    Card *card = [conekta.Card initWithNumber: @"credit_card_number" name: @"owner_credit_card" cvc: @"000" expMonth: @"01" expYear: @"2020"];
+    
+    Token *token = [conekta.Token initWithCard:card];
+    
+    [token createWithSuccess: ^(NSDictionary *data) {
+        NSLog(@"%@", data);
+    } andError: ^(NSError *error) {
+        NSLog(@"%@", error);
+    }];
 }
 
 @end

231e559fba01f1244b33939b69f9be63b3fad55b
Galileo Guzmán <galileoguzman@gmail.com>
2015-11-28 09:43:09 -0600

    Initial Commit

---
+++ Conekta Token/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Conekta Token
+//
+//  Created by Galileo Guzman on 28/11/15.
+//  Copyright © 2015 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
